6664b5804a815e90e9de4e46747127e2c460c96f
Luiz Carvalho <maximusmano@gmail.com>
2018-11-14 11:56:10 -0300

    handle watermak, seq and add @read attribute to class ServiceMessage

--- lib/stealth/services/facebook/events/message_reads_event.rb
+++ lib/stealth/services/facebook/events/message_reads_event.rb
@@ -1,6 +1,8 @@
 # coding: utf-8
 # frozen_string_literal: true
 
+
+
 module Stealth
   module Services
     module Facebook
@@ -21,10 +23,27 @@ module Stealth
         private
 
           def fetch_read
-            service_message.payload = params['read']
+            service_message.read = { watermark: get_timestamp, seq: params['read']['seq'] }
+          end
+
+          def get_timestamp
+            Time.at(params['read']['watermark']/1000).to_datetime
           end
       end
 
+      module ReadsEvent
+        attr_accessor :read
+        def initialize(service:)
+          @read = {}
+          super
+        end
+      end
+
     end
+  end
+
+
+  class ServiceMessage
+    prepend Services::Facebook::ReadsEvent
   end
 end
